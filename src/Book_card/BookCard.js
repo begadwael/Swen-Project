aad91dc13396ba7cf11aabe84335cdb852704ae9
bashirok <bok8285@rit.edu>
2023-12-01 12:36:21 +0400

    rewrote BookCard and made it dynamic

--- src/Book_card/BookCard.js
+++ src/Book_card/BookCard.js
@@ -1,18 +1,77 @@
 import React from 'react';
 import './BookCard.css'
+import books from '../db/books.json';
+
 const BookCard = (props) => {
-    return (
-        <div className={"BookCard"}>
-            {/*kids book img*/}
-            <img onClick={event => {
-                window.location.href='/book';
-            }} className={"Book_img"} src="https://m.media-amazon.com/images/I/71TEJjGgHnL._SL1500_.jpg" alt="kids book"/>
-            {/*kids book title*/}
-            <h2 className={"Book_title"}>Dog Man 10: Mothering Heights (The New Blockbusting International Bestseller)</h2>
-            {/*kids book author*/}
-            <h3 className={"Book_author"}>Dav Pilkey</h3>
-        </div>
-    );
+    // hard coded variables for back up (begad you can change these if you want)
+    const title = "Kids Book";
+    const author = "Kids Author";
+    const bookImage = "https://images-na.ssl-images-amazon.com/images/I/51ZU%2BZ7ZJTL._SX258_BO1,204,203,200_.jpg";
+
+    // making the catalog dynamic
+    const book1 = books.find(book => book.id === 0);
+    const book2 = books.find(book => book.id === 1);
+    const book3 = books.find(book => book.id === 2);
+    const selecedBook =  props.id;
+    if (selecedBook === 0) {
+        return (
+            <div className={"BookCard"}>
+                {/*kids book img*/}
+                <img onClick={event => {
+                    window.location.href='/book';
+                }} className={"Book_img"} src={book1["img"]} alt="kids book"/>
+                {/*kids book title*/}
+                <h2 className={"Book_title"}>{book1["title"]}</h2>
+                {/*kids book author*/}
+                <h3 className={"Book_author"}>{book1["author"]}</h3>
+            </div>
+        );
+    }
+
+    else if (selecedBook === 1) {
+        return (
+            <div className={"BookCard"}>
+                {/*kids book img*/}
+                <img onClick={event => {
+                    window.location.href='/book';
+                }} className={"Book_img"} src={book2["img"]} alt="kids book"/>
+                {/*kids book title*/}
+                <h2 className={"Book_title"}>{book2["title"]}</h2>
+                {/*kids book author*/}
+                <h3 className={"Book_author"}>{book2["author"]}</h3>
+            </div>
+        );
+    }
+
+    else if (selecedBook === 2) {
+        return (
+            <div className={"BookCard"}>
+                {/*kids book img*/}
+                <img onClick={event => {
+                    window.location.href='/book';
+                }} className={"Book_img"} src={book3["img"]} alt="kids book"/>
+                {/*kids book title*/}
+                <h2 className={"Book_title"}>{book3["title"]}</h2>
+                {/*kids book author*/}
+                <h3 className={"Book_author"}>{book3["author"]}</h3>
+            </div>
+        );
+    }
+    
+    else {
+        return (
+            <div className={"BookCard"}>
+                {/*kids book img*/}
+                <img onClick={event => {
+                    window.location.href='/book';
+                }} className={"Book_img"} src={bookImage} alt="kids book"/>
+                {/*kids book title*/}
+                <h2 className={"Book_title"}>{title}</h2>
+                {/*kids book author*/}
+                <h3 className={"Book_author"}>{author}</h3>
+            </div>)
+    }
+
 };
 
 export default BookCard;
